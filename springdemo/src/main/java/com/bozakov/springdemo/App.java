62ce9c4fbd0b2609e6ced86d1e06ad6bef0b4a51
bozakovp <bozakovp@BOZAKOVP3.emea.hpqcorp.net>
2018-01-17 15:00:25 +0200

    pc to mac

--- springdemo/src/main/java/com/bozakov/springdemo/App.java
+++ springdemo/src/main/java/com/bozakov/springdemo/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! second push1" );
+        System.out.println( "Hello World! second push12" );
     }
 }
